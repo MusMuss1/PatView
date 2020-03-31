d045cc13c2fea3e0d7b9510f41d72e5c156a8d8a
MusMuss1 <suppport.ydownload@gmail.com>
2020-03-31 13:53:19 +0200

    *add comments

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -45,12 +45,12 @@ public class Controller {
     final ArrayList<Image> pics = new ArrayList<>();
     //btnpath click event
     public void getpath(ActionEvent event) {
-        final DirectoryChooser dc = new DirectoryChooser();   //Create FileBrowser for Directory chose
-        Stage dia = (Stage) dialog.getScene().getWindow();          //New Stage (Neues Fenster)
+        final DirectoryChooser dc = new DirectoryChooser();                                                                 //Create FileBrowser for Directory chose
+        Stage dia = (Stage) dialog.getScene().getWindow();                                                                  //New Stage (neus Fenster)
         File dir = dc.showDialog(dia);
 
-        if (dir != null) {                                           //Check if item is selected or selected item is not empty
-            txtroot.setText(dir.getAbsolutePath());                 //print selected path to textbox
+        if (dir != null) {                                                                                                  //Check if item is selected or selected item is not empty
+            txtroot.setText(dir.getAbsolutePath());                                                                         //print selected path to TextBox
             //System.out.println(dir.getAbsolutePath());
             String path = dir.getAbsolutePath();
             File file = new File(path);
@@ -60,11 +60,11 @@ public class Controller {
         }
     }
 
-    public TreeItem<String> getNodesForDirectory(File path) {           //Returns a TreeItem representation of the specified directory
+    public TreeItem<String> getNodesForDirectory(File path) {                                                               //Returns a TreeItem representation of the specified directory
         TreeItem<String> root = new TreeItem<String>(path.getName());
         for (File f : path.listFiles()) {
             System.out.println("Loading " + f.getName());
-            if (f.isDirectory()) {                                       //call the function recursively to put directories and files into TreeView
+            if (f.isDirectory()) {                                                                                          //call the function recursively to put directories and files into TreeView
                 root.getChildren().add(getNodesForDirectory(f));
             } else {
                 root.getChildren().add(new TreeItem<String>(f.getName()));
@@ -72,46 +72,46 @@ public class Controller {
         }
         return root;
     }
-    public String getitempath() {                                       //get path from selected TreeView item and return path type string
-        StringBuilder pathBuilder = new StringBuilder();                //create StringBuilder to get the path from type string
+    public String getitempath() {                                                                                           //get path from selected TreeView item and return path type string
+        StringBuilder pathBuilder = new StringBuilder();                                                                    //create StringBuilder to get the path from type string
         for (TreeItem<String> item = view.getSelectionModel().getSelectedItem();
-             item != null; item = item.getParent()) {                   // selected item may not be empty
+             item != null; item = item.getParent()) {                                                                       // selected item may not be empty
 
             pathBuilder.insert(0, item.getValue());
             pathBuilder.insert(0, "/");
         }
         String path = pathBuilder.toString();
-        System.out.println(path);                                       //write path to console (only checking)
-        if (path.contains(".pdf")){                                     //check if selected item is a PDF file
+        System.out.println(path);                                                                                           //write path to console (only checking)
+        if (path.contains(".pdf")){                                                                                         //check if selected item is a PDF file
             createImagesFromPDF(path);
         }
-        else{                                                           //if pdf = false call getimage() method
+        else{                                                                                                               //if pdf = false call getimage() method
             getimage(path);
         }
         return path;
     }
-    public void getimage(String path){                                  //create image from TreeView selected item
+    public void getimage(String path){                                                                                      //create image from TreeView selected item
         Image I = new Image("file:"+path);
         itemview.setImage(I);
     }
 
-    private List<BufferedImage> createImagesFromPDF(String path){       //convert PDF File to an Image using PDFBox | receives the path of type string
+    private List<BufferedImage> createImagesFromPDF(String path){                                                           //convert PDF File to an Image using PDFBox | receives the path of type string
 
-        List<BufferedImage> images = new ArrayList<BufferedImage>();                //create array to write the converted pdf pages as image into the array
-        pics.clear();
+        List<BufferedImage> images = new ArrayList<BufferedImage>();                                                                    //create array to write the converted pdf pages as image into the array
+        pics.clear();                                                                                                       //clear pics ArrayList<Image> to avoid problems with displaying PDF File in ImageView
         PDDocument pdf = null;
-        index = 0;
+        index = 0;                                                                                                          //set index to 0 to display 1st page of the PDF File when clicking on it
         try {
-            InputStream is = new FileInputStream(path);                 //InputStream to get the File (PDF File) from path
-            pdf = PDDocument.load(is);                                  //pdf requires type File which got from InputStream
+            InputStream is = new FileInputStream(path);                                                                     //InputStream to get the File (PDF File) from path
+            pdf = PDDocument.load(is);                                                                                      //pdf requires type File which got from InputStream
             PDFRenderer pdfRenderer = new PDFRenderer(pdf);
-            for (int page = 0; page < pdf.getNumberOfPages(); ++page) {                                 //Create PDF Renderer (PDFBox) and render all PDF Pages to BufferedImages
+            for (int page = 0; page < pdf.getNumberOfPages(); ++page) {                                                     //Create PDF Renderer (PDFBox) and render all PDF Pages to BufferedImages
                 BufferedImage img = pdfRenderer.renderImageWithDPI(page, 300, ImageType.RGB);
-                images.add(img);                                                                        //add buffered to the created array list
-                WritableImage writable = SwingFXUtils.toFXImage(images.get(page), null);                //create a writable image from buffered image, to write it to ImageView
-                pics.add(writable);
-                itemview.setImage(pics.get(index));                                                            //set image to ImageView
-                System.out.println(images.size());
+                images.add(img);                                                                                            //add buffered to the created array list
+                WritableImage writable = SwingFXUtils.toFXImage(images.get(page), null);                       //create a writable image from buffered image, to write it to ImageView
+                pics.add(writable);                                                                                         //add writable image to new ArrayList "pics"
+                itemview.setImage(pics.get(index));                                                                         //set image to ImageView
+                System.out.println(images.size());                                                                          //print size of pics ArrayList (only checking)
             }
         }
         catch (IOException e) {
@@ -125,7 +125,8 @@ public class Controller {
         }
         return images;
     }
-    public void goforward(ActionEvent event){
+    //brnforward click event
+    public void goforward(ActionEvent event){                                                                               //"goforwad" method handles btnforward click event
         try{
             if(index<pics.size()-1){
                 index++;
@@ -138,7 +139,8 @@ public class Controller {
 
         }
     }
-    public void goback(ActionEvent event){
+    //btnback click event
+    public void goback(ActionEvent event){                                                                                  //"goback" mehtod handles btnback click event
         try{
             if(index>0){
                 index--;
