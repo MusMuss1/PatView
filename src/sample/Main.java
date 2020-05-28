2500653656a83b87561b02a2a18a71556157417d
MusMuss1 <suppport.ydownload@gmail.com>
2020-05-28 16:57:54 +0200

    Changes *add Logo

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
         primaryStage.setTitle("PatView");
-        //primaryStage.getIcons().add(new Image("logo.png"));
+        primaryStage.getIcons().add(new Image("logo.png"));
         //primaryStage.setScene(new Scene(root, 1280, 720));
         root.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
         primaryStage.setScene(new Scene(root));
